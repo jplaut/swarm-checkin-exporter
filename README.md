6670ea3549ce4d4eed9d70607c9cf4a30cb5a65c
Jon Plaut <jplaut88@gmail.com>
2025-02-13 18:14:58 -0800

    finished exporter script

---
+++ README.md
@@ -0,0 +1,17 @@
+# Swarm Checkin Exporter
+
+### Description
+This is a simple script to export Swarm checkins to a CSV. The CSVs are split into groups of 2000 so that they can then be uploaded to [Google My Maps](https://www.google.com/maps/d/u/0/) (see My Maps upload instructions [here](https://support.google.com/mymaps/answer/3024836)).
+
+### Setup
+The Swarm API NPM package didn't work for this tool because it could only fetch checkins since a certain timestamp, not by offset. Given the API's impicit 100 checkin limit, it would have been hard to fetch all records. Also, Swarm doesn't have an API dashboard to create client ID/Secret.
+
+I ended up reverse engineering the Swarm website and found a request on the page that included the `OAUTH_TOKEN` and `USER_ID`. To find yours, login to your Swarm account on the web, open the web inspector, scroll down the checkin list until the page loads more checkins, and find the request. The token and userId are there (eg `https://api.foursquare.com/v2/users/${USER_ID}/historysearch?...oauth_token=${OAUTH_TOKEN}`).
+
+Add those to the `.env.example` file and rename it `.env`.
+
+### Running the script
+`npm run main`
+
+### Output
+The script outputs files called `export_${i}.csv`.
---
+++ index.js
@@ -0,0 +1,55 @@
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const axios = require('axios');
+require('dotenv').config()
+
+const VERSION = new Date().toISOString().slice(0,10).replace(/-/g,"");
+
+const createCSVWriter = (i) => (
+  createCsvWriter({
+    path: `export_${i}.csv`,
+    header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'address', title: 'ADDRESS'},
+        {id: 'latlng', title: 'LAT/LNG'}
+    ]
+  })
+);
+
+const createRows = (checkins) => (
+  checkins.map(checkin => ({
+     name: checkin.venue.name,
+     // need to replace cross streets (eg 253 W 51st St (btwn Broadway & 8th Ave))
+     address: checkin.venue.location.formattedAddress.join(', ').replace(/ \([^)]*\)/g, ''),
+    latlng: `${checkin.venue.location.lat},${checkin.venue.location.lng}`
+  }))
+)
+
+async function main() {
+  let totalCount = 1, csvIndex = 1, csvWriter = createCSVWriter(csvIndex), checkinRequest;
+
+  const params = {
+    v: VERSION,
+    offset: 0,
+    oauth_token: process.env.OAUTH_TOKEN
+  }
+
+  while (params.offset < totalCount) {
+    checkinRequest = await axios.get(`https://api.foursquare.com/v2/users/${process.env.USER_ID}/historysearch`, { params });
+    totalCount = checkinRequest.data.response.checkins.count;
+    console.log(`Exporting ${params.offset} - ${params.offset + checkinRequest.data.response.checkins.items.length} of ${totalCount}`);
+    params.offset += checkinRequest.data.response.checkins.items.length;
+
+    // limit each CSV to 2000 so upload to My Maps works
+    if (params.offset > csvIndex * 2000) {
+      csvIndex += 1;
+      csvWriter = createCSVWriter(csvIndex);
+    }
+
+    await csvWriter.writeRecords(createRows(checkinRequest.data.response.checkins.items));
+    
+  }
+  
+  console.log(`Completed exporting ${params.offset} checkins.`);
+}
+
+main();
